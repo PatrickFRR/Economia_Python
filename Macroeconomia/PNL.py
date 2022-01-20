1f8fd555ad6581dc207a86cbecb0ac53e9132cc1
Patrick F R Ribeiro <87321768+PatrickFRR@users.noreply.github.com>
2022-01-19 23:13:46 -0300

    Add files via upload

--- Macroeconomia/PNL.py
+++ Macroeconomia/PNL.py
@@ -1,8 +1,8 @@
-# Com base nas informações a seguir calcule o PNL(Produto Nacional Líquido):
+# Com base nas informações a seguir calcule o valor das exportações líquidas, o PIB e o PNL:
 # Compras governamentais $120 bilhões, Depreciação $40 bilhões, Consumo	$400 bilhões,
 # Investimento em negócios $60 bilhões, Exportações $100 bilhões, Importações $120 bilhões,
 # Receita vinda do resto do mundo $10 bilhões e Pagamentos para o resto do mundo $8 bilhões.
-
+import pandas as pd
 
 #Calculando o PNL
 c = float(input('Digite o total de consumo: $ '))
@@ -10,7 +10,7 @@ i = float(input('Digite o total de investimento: $ '))
 g = float(input('Digite o total de compras do governo: $ '))
 x = float(input('Digite o total de exportações: $ '))
 m = float(input('Digite o total de importações: $ '))
-rm = float(input('Digite o total de receitas vinda do resto do mundo: $ '))
+rrm = float(input('Digite o total de receitas vinda do resto do mundo: $ '))
 prm = float(input('Digite o total de pagamentos para o resto do mundo: $ '))
 d = float(input('Digite o total de depreciação: $ '))
 #calculando exportações líquidas
@@ -19,7 +19,17 @@ t = x - m
 pib = c + i + g + t
 
 #calculando o PNL
-pnl = pib + rm - prm - d
-print('O seu PIB é de ${:0f} e o seu PNL é de $ {:.0f} na moeda local.'.format(pib, pnl))
+pnl = pib + rrm - prm - d
 
+#Criando um dicionário DataFrame
+data = {
+'Componentes': ['Compras governamentais', 'Depreciação', 'Consumo', 'Investimento em negócios', 'Exportações', 'Importações', 'Receita vinda do resto do mundo', 'Pagamentos para o resto do mundo'],
+'Valores em Bilhões': ['$ {}'.format(g), '$ {}'.format(d), '$ {}'.format(c), '$ {}'.format(i), '$ {}'.format(x), '$ {}'.format(m), '$ {}'.format(rrm), '$ {}'.format(prm)]
+}
+#Criando o DataFrame
+df = pd.DataFrame(data, columns=['Componentes', 'Valores em Bilhões'])
+print(df)
+print('O valor das exportações líquidas é de ${:0f} bilhões na moeda local.'.format(t))
+print('O seu PIB é de ${:0f} trilhões na moeda local.'.format(pib))
+print('O seu PNL é de $ {:.0f} trilhões na moeda local.'.format(pnl))
 
--- Macroeconomia/Producao_PIB.py
+++ Macroeconomia/Producao_PIB.py
@@ -1,6 +1,7 @@
 #O país A tem Bens duráveis de $2.9 bilhões, Bens não duráveis de $ 2.3 bilhões,
 # Serviços de $ 10.8 bilhões, Estrutura de $ 1.3 bilhões e
-# Variação nos estoques de $ 0.1 bilhões. Qual é o valor em dólar do PIB?
+# Variação nos estoques de $ 0.1 bilhões. Qual é o valor em moeda local do PIB?
+import pandas as pd
 
 #Calculando o PIB pela ótica da produção
 bd = float(input('Digite o total de Bens durávies: $ '))
@@ -12,4 +13,13 @@ v = float(input('Digite o total da Variação nos estoques: $ '))
 #calculando PIB
 pib = bd + bn + s + e + v
 
-print('O seu PIB,  pela ótica da produção, foi de $ {:.1f} na moeda local.'.format(pib))
+#Criando um dicionário DataFrame
+data = {
+'Componentes': ['Bens duráveis', 'Bens não duráveis', 'Serviços', 'Estruturas', 'Variação nos estoques', 'PIB total'],
+'Fornecedor': ['$ {}'.format(bd), '$ {}'.format(bn), '$ {}'.format(s), '$ {}'.format(e), '$ {}'.format(v), '$ {}'.format(pib)]
+}
+#Criando o DataFrame
+df = pd.DataFrame(data, columns=['Componentes', 'Fornecedor'])
+print(df)
+
+print('O seu PIB, pela ótica da produção, foi de $ {:.1f} trilhões na moeda local.'.format(pib))
